5c3bff8b535e788b6c70904c34d28edcd14d339f
dnjsxorla@naver.com <taewonkim168@gmail.com>
2020-11-20 18:01:00 +0900

    20201120

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = "rooms"
+app_name = "core"
 
 urlpatterns = [path("list", views.list_rooms)]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from rooms.models import Room
 
 # Create your views here.
+
+
+def list_rooms(requeset):
+    pass
